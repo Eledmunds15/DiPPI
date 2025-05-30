82a685de25e147532ccb167710d70008c46075be
Eledmunds15 <eledmunds1@sheffield.ac.uk>
2025-05-30 16:05:58 +0100

    Updating run script for HPC

--- run.sh
+++ run.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 #SBATCH --job-name=Precipitate_Analysis
 #SBATCH --mail-user=eledmunds1@sheffield.ac.uk
-#SBATCH --time=01:00:00
-#SBATCH --mem-per-cpu=750MB
+#SBATCH --time=00:10:00
+#SBATCH --mem-per-cpu=1500MB
 #SBATCH --cpus-per-task=1
-#SBATCH --ntasks=16
+#SBATCH --ntasks=256
 #SBATCH --output=output-%j.log
 #SBATCH --error=error-%j.log
+
+# Create or clear HPC_outputs directory
+if [ -d HPC_outputs ]; then
+    rm -rf HPC_outputs/*
+else
+    mkdir HPC_outputs
+fi
 
 # Load python modules
 export SLURM_EXPORT_ENV=ALL
 module load Anaconda3
-source activate analysis
+conda activate mol_dynamics_lmp
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
